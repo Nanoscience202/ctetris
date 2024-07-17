555b1713f7f4d9f1baf4e08a75a3c5419dd20ca8
Nanoscience202 <vincentmingli@gmail.com>
2024-07-17 19:29:36 -0400

    misc: updated gitignore

--- main.c
+++ main.c
@@ -1,7 +1,31 @@
 #include <ncurses.h>
+#include <stdbool.h>
+
+void init();
+void quit();
 
 int main(void) {
-  printf("Hello world");
+  init();
 
+  int cols = 10;
+  int rows = 20;
+
+  quit();
   return 0;
+}
+
+void init() {
+  initscr();
+  cbreak();
+  keypad(stdscr, true);
+  clear();
+  refresh();
+}
+
+void quit() {
+  clear();
+  printw("Press any key to exit");
+  refresh();
+  getch();
+  endwin();
 }
